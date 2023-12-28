13e4d141320b81bed124a068b6b1f201e19be4a9
aniket-git-hub <singhdharmvir81@gmail.com>
2023-12-28 18:12:05 +0530

    update

--- validators/authValidators.js
+++ validators/authValidators.js
@@ -1,14 +1,43 @@
 import { body } from 'express-validator';
 
 export const registerValidation = [
-    body('firstName').trim().notEmpty().withMessage('First name is required'),
-    body('lastName').trim().notEmpty().withMessage('Last name is required'),
-    body('email').isEmail().withMessage('Invalid email address'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    body('mobileNumber').isMobilePhone().withMessage('Invalid mobile number'),
+    body('firstName')
+        .trim()
+        .notEmpty().withMessage('First name is required')
+        .isLength({ max: 25 }).withMessage('First name should not be longer than 25 characters'),
+
+    body('lastName')
+        .trim()
+        .notEmpty().withMessage('Last name is required')
+        .isLength({ max: 25 }).withMessage('Last name should not be longer than 25 characters'),
+
+    body('email')
+        .trim()
+        .isEmail().withMessage('Invalid email address'),
+
+    body('password')
+        .trim()
+        .isLength({ min: 6, max: 30 }).withMessage('Password must be between 6 and 30 characters')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)
+        .withMessage('Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character'),
+
+    body('mobileNumber')
+        .trim()
+        .custom((value) => {
+            const regex = /^[6-9]\d{9}$/;
+            if (!regex.test(value)) {
+                throw new Error('Invalid Indian mobile number');
+            }
+            return true;
+        }),
 ];
 
 export const loginValidation = [
-    body('email').isEmail().withMessage('Invalid email address'),
-    body('password').notEmpty().withMessage('Password is required'),
+    body('email')
+        .trim()
+        .isEmail().withMessage('Invalid email address'),
+
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 6, max: 30 }).withMessage('Password must be between 6 and 30 characters'),
 ];
